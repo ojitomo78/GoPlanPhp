07b039674fde9ae0e819422c782c911797c23337
Ojitomo <87612008+ojitomo78@users.noreply.github.com>
2025-10-08 11:08:33 -0500

    Update insertar_cliente.php

--- public/insertar_cliente.php
+++ public/insertar_cliente.php
@@ -2,6 +2,8 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
+date_default_timezone_set('America/Bogota');
+
 $servername = "yamabiko.proxy.rlwy.net";
 $username   = "root";
 $password   = "ExNlzRDrivHkvSmhToLKgUJTSLPFklcD";
